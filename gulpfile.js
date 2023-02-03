c852185d1ba788f1d6db75dfc28e121fce12bce4
Francisco Berti da Cruz <fbc991@live.com>
2023-02-03 11:05:36 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// function teste(t) {
+//     console.log("funcionando")
+//     t()
+// }
+
+// exports.default = teste;
+
+const gulp = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+
+function styles() {
+    return gulp.src("./src/styles/*.scss")
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(gulp.dest("./dist/css"))
+}
+
+exports.default = styles
+exports.watch = function () {
+    gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+}
